8486ff2c6d0a5585875b352b1a171d8c19641ffd
COPART\gamutha <gauri.mutha@gmail.com>
2017-05-19 15:29:17 -0500

    gm 19may

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,25 +1,9 @@
-CREATE TABLE Car (
-  id    INT AUTO_INCREMENT PRIMARY KEY,
-  make  VARCHAR(64),
-  model VARCHAR(64),
-  year  VARCHAR(32),
-  color VARCHAR(32),
-  ownerId INT REFERENCES Owner
-
-);
-CREATE TABLE Owner (
-  ownerId    INT AUTO_INCREMENT PRIMARY KEY,
-  name  VARCHAR(64)
-);
+INSERT INTO OWNER(ownerId,name) VALUES (1,'Gauri');
+INSERT INTO OWNER(ownerId,name) VALUES (2,'Anant');
 
 
-INSERT INTO Car(make, model, year, color) VALUES ('a', 'b', '2000', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('b', 'b', '2001', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('c', 'b', '2002', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('d', 'b', '2003', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('e', 'b', '2004', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('f', 'b', '2005', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('g', 'b', '2006', 'd');
-INSERT INTO Car(make, model, year, color) VALUES ('h', 'b', '2007', 'd');
-
-INSERT INTO (make, model, year, color) VALUES ('h', 'b', '2007', 'd');
+INSERT INTO Car(make, model, year, color,ownerId) VALUES ('AUDI', 'Model1', '1998', 'Black',1);
+INSERT INTO Car(make, model, year, color,ownerId) VALUES ('AUDI', 'Model1', '2001', 'Black',1);
+INSERT INTO Car(make, model, year, color,ownerId) VALUES ('MUSTANG', 'Model1', '2002', 'Red',2);
+INSERT INTO Car(make, model, year, color,ownerId) VALUES ('FERRARI', 'Model1', '2003', 'Red',2);
+INSERT INTO Car(make, model, year, color,ownerId) VALUES ('MERZ', 'Model1', '2004', 'Silver',1);
