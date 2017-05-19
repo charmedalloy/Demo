57c8423583ea390b21c0cf58920df3ecf740c540
COPART\gamutha <gauri.mutha@gmail.com>
2017-05-19 14:29:42 -0500

    Gauri Changes 19May

--- src/main/java/com/car/CarRepository.java
+++ src/main/java/com/car/CarRepository.java
@@ -6,11 +6,24 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Repository
 public interface CarRepository extends CrudRepository<Car,String> {
 
+	
     public List<Car> findByMakeInAndYearIn(String make, String year);
+    
 
     @Query(nativeQuery=true, value="SELECT * FROM Car c where c.id < 5")
     public List<Car> findCar();
+    
+    @Query("select c.make as make, c.model as model "
+			+ "from Car c where c.owner.ownerid = ? group by c.make,c.model")
+	List<Car> findByOwnerId(int ownerId);
+
+	public List<Car> findCarNew();
+    
+    
 }
